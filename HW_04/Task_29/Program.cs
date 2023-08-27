b9402952084f6149d5dca466a90c706867ac2147
Dmitrii <zatsepindm@gmail.com>
2023-08-27 14:06:05 +0300

    Initial commit

---
+++ HW_04/Task_29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов случайных целых чисел от 1 до 50 и выводит их на экран.
+
+int[] Makearray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(1, 51);
+    }
+    return array;
+}
+void Printarray(int[] A)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        System.Console.Write($"{A[i]} ");
+    }
+    System.Console.WriteLine();
+}
+int[] arr = Makearray(); 
+Printarray(arr); 
+
