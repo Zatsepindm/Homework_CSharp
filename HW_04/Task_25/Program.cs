5d43de1a56818468b7f4e1fe901c4a0b0d3c11ee
Dmitrii <zatsepindm@gmail.com>
2023-08-27 14:50:35 +0300

    Initial commit

---
+++ HW_04/Task_25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+void Expo(int A, int B)
+{
+    int expo = 1;
+    if (A == 0 && B == 0)
+        System.Console.WriteLine($"Ошибка. Введите другие значения");
+    else
+    {
+        for (int i = 1; i <= B; i++)
+        {
+            expo = expo * A;
+        }
+        System.Console.WriteLine(expo);
+    }
+}
+
+System.Console.WriteLine("Введите основание степени");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите показатель степени");
+int B = Convert.ToInt32(Console.ReadLine());
+Expo(A, B);
